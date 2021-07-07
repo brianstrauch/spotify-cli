c8196ad2dfaacf18e3e083aa150e6c57da4d4381
Urban Cetinski <urbanc@users.noreply.github.com>
2021-07-07 13:58:02 +0200

    Update internal/login/login.go -make staticcheck happy without err check
    Co-authored-by: Brian Strauch <bstrauch24@gmail.com>

--- internal/login/login.go
+++ internal/login/login.go
@@ -119,10 +119,7 @@ func listenForCode(state string) (code string, err error) {
 
 		// Use a separate thread so browser doesn't show a "No Connection" message
 		go func() {
-			 err := server.Shutdown(context.TODO())
-			 if err != nil {
-				 fmt.Printf("Server shutdown error.")
-			 }
+			 _ = server.Shutdown(context.TODO())
 		}()
 	})
 
