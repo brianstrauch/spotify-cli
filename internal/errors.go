407d949f4a1c2a218870797013ff61a92557e7bc
Theo Andresier <49570057+Threpio@users.noreply.github.com>
2021-07-21 17:58:26 +0200

    Rename errors as per suggestion
    Co-authored-by: Brian Strauch <bstrauch24@gmail.com>

--- internal/errors.go
+++ internal/errors.go
@@ -4,9 +4,9 @@ const (
 	ErrAlreadyUpToDate     = "Already up to date"
 	ErrLoginFailed         = "Login failed"
 	ErrNoActiveDevice      = "Player command failed: No active device found"
-	ErrNoAlbums            = "No albums found"
-	ErrNoDevices           = "No devices"
-	ErrNoPlaylists         = "No playlists"
+	ErrNoAlbumsFound            = "No albums found"
+	ErrNoDevicesFound           = "No devices found"
+	ErrNoPlaylistsFound         = "No playlists found"
 	ErrNoPrevious          = "No track before this one"
 	ErrNotLoggedIn         = `You are not logged in: Run "spotify login"`
 	ErrRestrictionViolated = "Player command failed: Restriction violated"
