fb37948a733d1c89f61ca12faf4ade2fe5b18bc2
Theo Andresier <49570057+Threpio@users.noreply.github.com>
2021-07-26 14:21:05 +0200

    Update internal/errors.go
    Co-authored-by: Brian Strauch <bstrauch24@gmail.com>

--- internal/errors.go
+++ internal/errors.go
@@ -4,9 +4,9 @@ const (
 	ErrAlreadyUpToDate     = "Already up to date"
 	ErrLoginFailed         = "Login failed"
 	ErrNoActiveDevice      = "Player command failed: No active device found"
-	ErrNoAlbumsFound            = "No albums found"
-	ErrNoDevicesFound           = "No devices found"
-	ErrNoPlaylistsFound         = "No playlists found"
+	ErrNoAlbumsFound       = "No albums found"
+	ErrNoDevicesFound      = "No devices found"
+	ErrNoPlaylistsFound    = "No playlists found"
 	ErrNoPrevious          = "No track before this one"
 	ErrNotLoggedIn         = `You are not logged in: Run "spotify login"`
 	ErrRestrictionViolated = "Player command failed: Restriction violated"
